ba9018e676a11c0771bd059393315a496b193875
my4wyy <130785414+my4wyy@users.noreply.github.com>
2024-08-22 21:44:23 -0300

    Update and rename a to MainApp.java

---
+++ Implementação/src/models/MainApp.java
@@ -0,0 +1,10 @@
+public class MainApp {
+    public static void main(String[] args) {
+      
+        
+        System.out.println("Bem-vindo ao Sistema de Matrículas!");
+        
+        
+    }
+}
+
