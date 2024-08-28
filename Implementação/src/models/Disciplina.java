31964e208914a8b46ff2c5542f916217c6c764ee
mariaeduardagsf <129904639+mariaeduardagsf@users.noreply.github.com>
2024-08-27 22:26:36 -0300

    Update Disciplina.java

--- Implementação/src/models/Disciplina.java
+++ Implementação/src/models/Disciplina.java
@@ -1,9 +1,12 @@
 package models;
 
 import enums.TipoDisciplina;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Disciplina {
+public class Disciplina implements Serializable {
     private String codigo;
     private String nome;
     private int creditos;
@@ -11,90 +14,54 @@ public class Disciplina {
     private List<Disciplina> preRequisitos;
     private int vagasDisponiveis;
     private List<Aluno> alunosMatriculados;
-    private Professor professorResponsavel;
 
     public Disciplina(String codigo, String nome, int creditos, TipoDisciplina tipo, int vagasDisponiveis) {
         this.codigo = codigo;
         this.nome = nome;
         this.creditos = creditos;
         this.tipo = tipo;
+        this.preRequisitos = new ArrayList<>();
         this.vagasDisponiveis = vagasDisponiveis;
+        this.alunosMatriculados = new ArrayList<>();
     }
 
     public String getCodigo() {
         return codigo;
     }
 
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public int getCreditos() {
         return creditos;
     }
 
-    public void setCreditos(int creditos) {
-        this.creditos = creditos;
-    }
-
     public TipoDisciplina getTipo() {
         return tipo;
-    }
-
-    public void setTipo(TipoDisciplina tipo) {
-        this.tipo = tipo;
-    }
-
-    public List<Disciplina> getPreRequisitos() {
-        return preRequisitos;
-    }
-
-    public void setPreRequisitos(List<Disciplina> preRequisitos) {
-        this.preRequisitos = preRequisitos;
     }
 
     public int getVagasDisponiveis() {
         return vagasDisponiveis;
     }
 
-    public void setVagasDisponiveis(int vagasDisponiveis) {
-        this.vagasDisponiveis = vagasDisponiveis;
+    public void matricularAluno(Aluno aluno) {
+        if (vagasDisponiveis > 0) {
+            alunosMatriculados.add(aluno);
+            vagasDisponiveis--;
+        }
+    }
+
+    public void cancelarMatriculaAluno(Aluno aluno) {
+        alunosMatriculados.remove(aluno);
+        vagasDisponiveis++;
+    }
+
+    public void encerrarInscricoes() {
+        System.out.println("Inscrições encerradas para a disciplina: " + nome);
     }
 
     public List<Aluno> getAlunosMatriculados() {
         return alunosMatriculados;
-    }
-
-    public void setAlunosMatriculados(List<Aluno> alunosMatriculados) {
-        this.alunosMatriculados = alunosMatriculados;
-    }
-
-    public Professor getProfessorResponsavel() {
-        return professorResponsavel;
-    }
-
-    public void setProfessorResponsavel(Professor professorResponsavel) {
-        this.professorResponsavel = professorResponsavel;
-    }
-
-    public void matricularAluno(Aluno aluno) {
-    }
-
-    public void cancelarMatriculaAluno(Aluno aluno) {
-    }
-
-    public int verificarVagas() {
-        return 0;
-    }
-
-    public void encerrarInscricoes() {
-    }
+}
 }
