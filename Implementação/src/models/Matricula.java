255ef744ffcb4421cf1de9d644b56fcbff5550cd
my4wyy <130785414+my4wyy@users.noreply.github.com>
2024-08-27 22:26:03 -0300

    Create Matricula.java

---
+++ Implementação/src/models/Matricula.java
@@ -0,0 +1,47 @@
+package models;
+
+import java.util.Date;
+
+public class Matricula {
+    private Aluno aluno;
+    private Disciplina disciplina;
+    private Date dataMatricula;
+
+    public Matricula(Aluno aluno, Disciplina disciplina) {
+        this.aluno = aluno;
+        this.disciplina = disciplina;
+        this.dataMatricula = new Date();
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public Date getDataMatricula() {
+        return dataMatricula;
+    }
+
+    public void setDataMatricula(Date dataMatricula) {
+        this.dataMatricula = dataMatricula;
+    }
+
+    public void efetuarMatricula() {
+        aluno.matricularDisciplina(disciplina);
+    }
+
+    public void cancelarMatricula() {
+        aluno.cancelarMatricula(disciplina);
+    }
+}
