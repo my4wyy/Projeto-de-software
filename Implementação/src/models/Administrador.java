263ea7d51529c7a9bc4da7aa4022f3a5b2e99b70
my4wyy <130785414+my4wyy@users.noreply.github.com>
2024-08-27 22:24:47 -0300

    Update Administrador.java

--- Implementação/src/models/Administrador.java
+++ Implementação/src/models/Administrador.java
@@ -1,72 +1,47 @@
 package models;
 
-import java.io.Serializable;
-
 import enums.TipoUsuario;
 
-public class Administrador extends Usuario implements Serializable {
-    private String nome;
-    private String matricula;
-    private String departamento;
+public abstract class Usuario {
+    private String nomeUsuario;
+    private String senha;
+    private TipoUsuario papel;
 
-    public Administrador(String nomeUsuario, String senha, String nome, String matricula, String departamento) {
-        super(nomeUsuario, senha, TipoUsuario.ADMINISTRADOR);
-        this.nome = nome;
-        this.matricula = matricula;
-        this.departamento = departamento;
+    public Usuario(String nomeUsuario, String senha, TipoUsuario papel) {
+        this.nomeUsuario = nomeUsuario;
+        this.senha = senha;
+        this.papel = papel;
     }
 
-    public void notificarInscricao(Aluno aluno) {
-        System.out.println("Notificação: Inscrição realizada com sucesso para " + aluno.getNome());
+    public String getNomeUsuario() {
+        return nomeUsuario;
     }
 
-    public void cobrarDisciplinas(Aluno aluno) {
-        System.out.println("Notificação: Cobrança enviada para " + aluno.getNome() + " referente às disciplinas matriculadas.");
-    }
-}
-
-    public String getNome() {
-        return nome;
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String getSenha() {
+        return senha;
     }
 
-    public String getMatricula() {
-        return matricula;
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
+    public TipoUsuario getPapel() {
+        return papel;
     }
 
-    public String getDepartamento() {
-        return departamento;
+    public void setPapel(TipoUsuario papel) {
+        this.papel = papel;
     }
 
-    public void setDepartamento(String departamento) {
-        this.departamento = departamento;
+    public boolean fazerLogin(String nomeUsuario, String senha) {
+        return this.nomeUsuario.equals(nomeUsuario) && this.senha.equals(senha);
     }
 
-    public void gerenciarAluno(Aluno aluno) {
-        // Lógica para gerenciar aluno
+    public void acessarSistema() {
+        System.out.println("Acessando o sistema...");
     }
-
-    public void gerenciarProfessor(Professor professor) {
-        // Lógica para gerenciar professor
-    }
-
-    public void gerenciarDisciplina(Disciplina disciplina) {
-        // Lógica para gerenciar disciplina
-    }
-
-    public void encerrarInscricoes(Disciplina disciplina) {
-        disciplina.encerrarInscricoes();
-    }
-
-    public Curriculo gerarCurriculo(Aluno aluno) {
-        return aluno.gerarCurriculo();
-    }
-
 }
