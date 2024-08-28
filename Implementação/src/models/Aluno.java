ac268309ed1ec5ab246bc0b7d300f6558db4184d
migueltico2 <130784916+migueltico2@users.noreply.github.com>
2024-08-27 22:25:01 -0300

    Update Aluno.java

--- Implementação/src/models/Aluno.java
+++ Implementação/src/models/Aluno.java
@@ -1,66 +1,73 @@
 package models;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Aluno extends Usuario {
+import enums.TipoUsuario;
+
+public class Aluno extends Usuario implements Serializable {
     private String nome;
     private String matricula;
     private String curso;
-    private List<Disciplina> disciplinasMatriculadas;
+    private List<Matricula> matriculas;
+    private Administrador administrador;
 
     public Aluno(String nomeUsuario, String senha, String nome, String matricula, String curso) {
-        super(nomeUsuario, senha, enums.TipoUsuario.ALUNO);
+        super(nomeUsuario, senha, TipoUsuario.ALUNO);
         this.nome = nome;
         this.matricula = matricula;
         this.curso = curso;
+        this.matriculas = new ArrayList<>();
+        this.administrador = new Administrador("admin", "admin123", "Admin", "0001", "Administração"); // Admin associado
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getMatricula() {
-        return matricula;
-    }
-
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-
-    public String getCurso() {
-        return curso;
-    }
-
-    public void setCurso(String curso) {
-        this.curso = curso;
-    }
-
-    public List<Disciplina> getDisciplinasMatriculadas() {
-        return disciplinasMatriculadas;
-    }
-
-    public void setDisciplinasMatriculadas(List<Disciplina> disciplinasMatriculadas) {
-        this.disciplinasMatriculadas = disciplinasMatriculadas;
-    }
-
     public void inscreverSemestre() {
+        System.out.println(nome + " foi inscrito no novo semestre.");
     }
 
     public void matricularDisciplina(Disciplina disciplina) {
+        if (disciplina.getVagasDisponiveis() > 0) {
+            Matricula matricula = new Matricula(this, disciplina);
+            matriculas.add(matricula);
+            disciplina.matricularAluno(this);
+            System.out.println("Matriculado na disciplina: " + disciplina.getNome());
+
+            // Notificação de cobrança
+            administrador.cobrarDisciplinas(this);
+
+        } else {
+            System.out.println("Não há vagas disponíveis na disciplina: " + disciplina.getNome());
+        }
     }
 
     public void cancelarMatricula(Disciplina disciplina) {
+        for (Matricula matricula : matriculas) {
+            if (matricula.getDisciplina().equals(disciplina)) {
+                matriculas.remove(matricula);
+                disciplina.cancelarMatriculaAluno(this);
+                System.out.println("Matrícula cancelada na disciplina: " + disciplina.getNome());
+                return;
+            }
+        }
+        System.out.println("Matrícula não encontrada para a disciplina: " + disciplina.getNome());
     }
 
-    public int verificarVagas(Disciplina disciplina) {
-        return 0;
+    public List<Matricula> getMatriculas() {
+        return matriculas;
+    }
+
+    public void verificarVagas(Disciplina disciplina) {
+        System.out.println("Vagas disponíveis na disciplina " + disciplina.getNome() + ": " + disciplina.getVagasDisponiveis());
     }
 
     public Curriculo gerarCurriculo() {
-        return null;
+        Curriculo curriculo = new Curriculo(this);
+        System.out.println("Currículo gerado para " + nome);
+        return curriculo;
     }
 }
