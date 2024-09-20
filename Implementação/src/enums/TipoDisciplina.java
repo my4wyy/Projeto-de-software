556bb0b7ac01033e155c2e6c1284b570bfad4933
my4wyy <130785414+my4wyy@users.noreply.github.com>
2024-09-20 18:37:01 -0300

    Delete Implementação/src directory

--- Implementação/src/enums/TipoDisciplina.java
+++
@@ -1,6 +0,0 @@
-package enums;
-
-public enum TipoDisciplina {
-    OBRIGATORIA,
-    OPTATIVA
-}
--- Implementação/src/enums/TipoUsuario.java
+++
@@ -1,7 +0,0 @@
-package enums;
-
-public enum TipoUsuario {
-    ALUNO,
-    PROFESSOR,
-    ADMINISTRADOR
-}
