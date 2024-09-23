24bd96b1de7f1e22de2a1fc79912f326de7b9be4
my4wyy <130785414+my4wyy@users.noreply.github.com>
2024-09-23 11:08:23 -0300

    Update Cliente.java

--- Sistema de Aluguel de Carros/Implementação/models/Cliente.java
+++ Sistema de Aluguel de Carros/Implementação/models/Cliente.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Cliente extends Usuario {
+public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
