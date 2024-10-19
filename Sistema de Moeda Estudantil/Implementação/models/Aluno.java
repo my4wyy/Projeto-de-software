3c80cebac1a6ef3840daa3b26ed408cd8c85f3ae
my4wyy <130785414+my4wyy@users.noreply.github.com>
2024-10-19 09:37:07 -0300

    Create Aluno.java

---
+++ Sistema de Moeda Estudantil/Implementação/models/Aluno.java
@@ -0,0 +1,67 @@
+package br.com.demo.regescweb.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "aluno")
+public class Aluno extends Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rg;
+    private String curso;
+
+    public Aluno() {}
+
+    public Aluno(String nome, String endereco, String email, String senha, String cpf, Conta conta, Instituicao instituicao, String rg, String curso) {
+        super(nome, endereco, email, senha, cpf, conta, instituicao);
+        this.rg = rg;
+        this.curso = curso;
+    }
+
+    // Métodos
+    public void trocarMoedas(Vantagem vantagem) {
+        if (getConta().getSaldo() >= vantagem.getCusto()) {
+            getConta().setSaldo(getConta().getSaldo() - vantagem.getCusto());
+            String codigo = vantagem.gerarCodigo();
+            vantagem.enviarEmailCupom(this, codigo);
+        }
+    }
+
+    public void escolherInstituicao(Instituicao instituicao) {
+        setInstituicao(instituicao);
+    }
+
+    // Getters e Setters
+
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
